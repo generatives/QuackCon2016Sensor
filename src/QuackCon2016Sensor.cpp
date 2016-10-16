0facf289660d6bbf2e4832d324140bdf13a04cbe
James <jamie8@shaw.ca>
2016-10-15 23:56:46 -0700

    Made the jerk Hardware sensor work, performed some calibration

--- src/QuackCon2016Sensor.cpp
+++ src/QuackCon2016Sensor.cpp
@@ -1,93 +1,103 @@
-/*
- * Author: Jessica Gomez <jessica.gomez.hernandez@intel.com>
- * Copyright (c) 2015 - 2016 Intel Corporation.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-/**
- * @file
- * @ingroup basic
- * @brief On board LED blink C++
- *
- * Blink the on board LED by writing a digital value to an output pin
- * using the IO mraa library.
- *
- * @date 29/09/2015
- */
 
 #include "mraa.hpp"
-
+#include "i2c.hpp"
+#include "aio.hpp"
+#include "gpio.hpp"
 #include <iostream>
 #include <unistd.h>
 
-/*
- * On board LED blink C++ example
- *
- * Demonstrate how to blink the on board LED, writing a digital value to an
- * output pin using the MRAA library.
- * No external hardware is needed.
- *
- * - digital out: on board LED
- *
- * Additional linker flags: none
- */
+#define I2C_Bus  6
+#define I2C_Address 0x4c
+
+int get_jerk(mraa::I2c* i2c, int axis)
+{
+	int x_value=0,x_accel=0,negated=0, a=0, b=0;
+	int jerk = 0;
+
+
+        //Read the Current Register Value
+    	x_value = i2c->readReg(0x02);
+    	//Mask the appropriate bits to determine alert
+    	negated = x_value & 0b00100000;
+    	x_accel = x_value & 0b00011111;
+    	//Negate if value is negative
+		if(!negated){x_accel = x_accel + 30;}
+		a = x_accel;
+		usleep(2000);
+		x_value = i2c->readReg(axis);
+    	negated = x_value & 0b00100000;
+    	x_accel = x_value & 0b00011111;
+    	//Negate if value is negative
+		if(!negated){x_accel = x_accel + 30;}
+		b = x_accel;
+
+		jerk = a-b;
+
+
+
+	return(jerk);
+}
 
 int main()
 {
-	// select onboard LED pin based on the platform type
-	// create a GPIO object from MRAA using it
-	mraa::Platform platform = mraa::getPlatformType();
-	mraa::Gpio* d_pin = NULL;
-	switch (platform) {
-		case mraa::INTEL_GALILEO_GEN1:
-			d_pin = new mraa::Gpio(3, true, true);
-			break;
-		case mraa::INTEL_GALILEO_GEN2:
-			d_pin = new mraa::Gpio(13, true, false);
-			break;
-		case mraa::INTEL_EDISON_FAB_C:
-			d_pin = new mraa::Gpio(13, true, false);
-			break;
-		default:
-			std::cerr << "Unsupported platform, exiting" << std::endl;
-			return mraa::ERROR_INVALID_PLATFORM;
-	}
-	if (d_pin == NULL) {
-		std::cerr << "Can't create mraa::Gpio object, exiting" << std::endl;
-		return mraa::ERROR_UNSPECIFIED;
-	}
+  int jerk_x = 0, jerk_y = 0, jerk_z= 0,jerk_Threshold_x = 55, jerk_Threshold_y = 50, jerk_Threshold_z = 50;
 
-	// set the pin as output
-	if (d_pin->dir(mraa::DIR_OUT) != mraa::SUCCESS) {
-		std::cerr << "Can't set digital pin as output, exiting" << std::endl;
-		return MRAA_ERROR_UNSPECIFIED;
-	}
+	mraa::I2c* i2c;
+    mraa::Gpio* buzzer;
+    mraa::Gpio* power;
+    mraa::Gpio* gnd;
+    i2c = new mraa::I2c(I2C_Bus,false);
+	buzzer = new mraa::Gpio(8);
+	buzzer->dir(mraa::DIR_OUT);
+	power = new mraa::Gpio(12);
+	power->dir(mraa::DIR_OUT);
+	power->write(1);
+	gnd = new mraa::Gpio(13);
+	gnd->dir(mraa::DIR_OUT);
+	gnd->write(0);
+	i2c->address(0x4c);
+	i2c->writeReg(0x07,0x01);
 
-	// loop forever toggling the on board LED every second
-	for (;;) {
-		d_pin->write(1);
-		sleep(5);
-		d_pin->write(0);
-		sleep(1);
-	}
+    int count=0;
+    while(1){
+    	count++;
+
+    	jerk_x = get_jerk(i2c,0x00);
+    	jerk_y = get_jerk(i2c,0x01);
+    	jerk_z = get_jerk(i2c,0x02);
+    if(jerk_x > jerk_Threshold_x)
+    {
+    	buzzer->write(1);
+    	usleep(50000);
+    	buzzer->write(0);
+    	std::clog << jerk_x;
+    	std::clog << "X";
+    	//usleep(5000);
+    }
+    if(jerk_y > jerk_Threshold_y)
+    {
+    	buzzer->write(1);
+    	usleep(50000);
+    	buzzer->write(0);
+    	std::clog << jerk_y;
+    	std::clog << "Y";
+    	//usleep(5000);
+    }
+    if(jerk_z > jerk_Threshold_z)
+    {
+    	buzzer->write(1);
+    	usleep(50000);
+    	buzzer->write(0);
+    	std::clog << jerk_z;
+    	std::clog << "Z";
+    	//usleep(5000);
+    }
+
+
+
+
+}
+
 
 	return mraa::SUCCESS;
 }
